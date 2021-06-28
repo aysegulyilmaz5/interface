bd45c24546934d2eca2752844f40005985d0e2cd
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-06-28 20:36:49 +0300

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,28 @@
+package jinterface;
+
+
+interface Field {
+	void getArea(int r,int PI);
+	void getSurround(int r,int PI);
+		
+}
+
+class Area implements Field{
+	public void getArea(int r,int PI) {
+		System.out.println("The area of the circle is"+(PI * r*r));
+	}
+	public void getSurround(int r,int PI) {
+		System.out.println("The circumferences of the circle is:"+(2*PI*r));
+	}
+}
+
+
+
+class MainClass{
+	public void main(String[] args) {
+		Area f1 = new Area();
+		Area f2 = new Area();
+		f1.getArea(10,3);
+		f2.getSurround(10,3);
+	}
+}
